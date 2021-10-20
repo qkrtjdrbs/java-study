428e910fe070075f6f2d8bd7b178f2a4bc2357dd
qkrtjdrbs <notlce@naver.com>
2021-10-20 20:41:13 +0900

    All beans

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -6,7 +6,6 @@ import hello.core.member.Member;
 import org.springframework.stereotype.Component;
 
 @Component
-@MainDiscountPolicy
 public class RateDiscountPolicy implements DiscountPolicy{
 
     private int discountPercent = 10;
